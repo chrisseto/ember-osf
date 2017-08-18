56e5832385becdbf0e53899f23b41cf5268dbf26
Abram Booth <abram@cos.io>
2017-08-18 14:27:29 -0400

    Clean up serializing relationships

--- addon/serializers/review-log.js
+++ addon/serializers/review-log.js
@@ -2,25 +2,8 @@ import OsfSerializer from './osf-serializer';
 import Ember from 'ember';
 
 export default OsfSerializer.extend({
-    serialize(snapshot) {
-        // Normal OSF serializer strips out relationships. We need to add back reviewable for this endpoint
-        const res = this._super(...arguments);
-        res.data.relationships = {};
-        for (var rel in snapshot.record._dirtyRelationships) {
-            let relationship = Ember.String.underscore(rel);
-            res.data.relationships[relationship] = {
-                data: {
-                    id: snapshot.belongsTo(rel, { id: true }),
-                    type: relTypes[rel]
-                }
-            };
-        }
-        return res;
-    }
-
+    // Serialize reviewable relationship
+    relationshipTypes: {
+        reviewable: 'preprints',
+    },
 });
-
-// Type mapping for relationship fields
-const relTypes = {
-    reviewable: 'preprints',
-};
